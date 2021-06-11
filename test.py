748710c08e4c52be216e76fca912ef6b890c1dd7
Debora <deborarebelatto1@gmail.com>
2021-06-11 09:15:56 -0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,13 @@
+
+days = int(input("Days: "))
+
+while days > 0:
+  print("Day", days)
+  tasks = int(input("Tasks: "))
+  doneTasks = input("Tasks: ")
+
+  for done in doneTasks:
+    print(done)
+
+  days -= 1
+
