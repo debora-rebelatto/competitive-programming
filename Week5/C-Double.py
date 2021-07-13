faa2fcd1ed1985814fe47ad907dbf03e50ba971f
Debora <deborarebelatto1@gmail.com>
2021-07-13 18:13:57 -0300

    13/07

---
+++ Week5/C-Double.py
@@ -0,0 +1,21 @@
+def com(a, b):
+    #gfg
+    dp = [[0 for k in range(len(b)+1)] for l in range(len(a)+1)]
+    res = 0
+    for i in range(len(a) + 1):
+        for j in range(len(b) + 1):
+            if (i == 0 or j == 0):
+                dp[i][j] = 0
+            elif (a[i-1] == b[j-1]):
+                dp[i][j] = dp[i-1][j-1] + 1
+                res = max(res, dp[i][j])
+            else:
+                dp[i][j] = 0
+    return res
+ 
+t=int(input())
+for _ in range(t):
+    a=input()
+    b=input()
+    x=com(a,b)
+    print(len(a)+len(b)-2*x)
